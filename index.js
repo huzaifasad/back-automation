10c4116aed50b427284f75291792fb9c462c1de1
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2024-05-27 11:51:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ app.post('/signup', async (req, res) => {
     const { name, email, password } = req.body;
     
     console.log('singup'+email)
-    try {
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-        user = new User({ name, email, password });  // Assuming no password hashing for demonstration purposes
-        await user.save();
-        const token = generateToken(user._id, email);
-        res.status(201).json({ email: user.email, token });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error registering new user');
-    }
+    // try {
+    //     let user = await User.findOne({ email });
+    //     if (user) {
+    //         return res.status(400).json({ message: "User already exists" });
+    //     }
+    //     user = new User({ name, email, password });  // Assuming no password hashing for demonstration purposes
+    //     await user.save();
+    //     const token = generateToken(user._id, email);
+    //     res.status(201).json({ email: user.email, token });
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).send('Error registering new user');
+    // }
 });
 
 // Login route
